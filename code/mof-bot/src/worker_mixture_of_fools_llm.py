4582f722f6c7831ac78f6a044aae490dd4f30cf0
Tim Cotten <tim@scryptedinc.com>
2024-10-30 22:59:28 -0400

    Pre-openai migrate completions with an example mixture prompt.

--- code/mof-bot/src/worker_mixture_of_fools_llm.py
+++ code/mof-bot/src/worker_mixture_of_fools_llm.py
@@ -1,9 +1,14 @@
 import os
 import openai
+from openai import OpenAI
+
 from typing import List, Dict
 from dotenv import load_dotenv
 
 LLM_MODEL_VERSION_MIN = "gpt-4o"
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def validate_api():
     """
@@ -12,7 +17,6 @@ def validate_api():
     Raises:
     - ValueError: If the API key or LLM model version is incorrect or missing, or if there's an issue connecting to OpenAI.
     """
-    load_dotenv()
 
     if not os.getenv("OPENAI_API_KEY"):
         raise ValueError(
@@ -24,34 +28,55 @@ def validate_api():
             "LLM_MODEL requires 'gpt-4o as a minimum. Please check your environment."
         )
 
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+
     llm_model = os.getenv("LLM_MODEL")
 
     # Fetch models and store information about the one we're using
     try:
-        available_models = [model.id for model in openai.Model.list().data]
+        available_models = [model.id for model in client.models.list().data]
         if llm_model and llm_model not in available_models:
             raise ValueError(
                 f"The model {llm_model} is not available or you don't have access to it."
             )
-    except openai.error.OpenAIError as e:
+    except openai.OpenAIError as e:
         raise ValueError(
             f"Failed to fetch the list of models from OpenAI: {str(e)}"
         )
-    
+
     print("OpenAI API access confirmed.")
 
 def try_mixture(posts, effects):
     validate_api()
+    
+    prompt = f"""
+Take the following two examples of writing from two different AI Chatbots and blend them into a synthesis. You will use a list of chosen effects to exaggerate your responses with a certain emotion, tone, and length.
 
+CONTENT A:
+
+{posts[0][1]}
+
+CONTENT B:
+
+{posts[1][1]}
+
+OBJECTIVES:
+* Write a tweet, choose the length to be around {effects.length} characters.
+* Choose an emotional tone of: {effects.emotion}.
+* Choose a writing style of: {effects.tone}.
+* You are allowed to swear, use vulgar language, or sexuality if desired.
+* You may not post stock or crypto tickers.
 """
-def _get_semantically_bound_chunk(self, txt):
-    content = self.prompt + "\n\n[INPUT JSON CONTENT]\n\n" + txt
-    max_tokens = self.window_size // 2
+
+    if effects.emojis:
+        prompt += "\n* You may add emojis"
+
+    print(f"DEBUGGING PROMPT: {prompt}")
+   
+    llm_model = os.getenv("LLM_MODEL")
+    content = prompt
 
     completion = openai.ChatCompletion.create(
-        model=self.llm_model,
-        max_tokens=max_tokens,
+        model=llm_model,
         temperature=1,
         top_p=1,
         frequency_penalty=0,
@@ -59,19 +84,12 @@ def _get_semantically_bound_chunk(self, txt):
         messages=[
             {
                 "role": "system",
-                "content": "The following is a conversation with an AI assistant labeled 'AI' with a user labeled 'Human'. The assistant is helpful, creative, clever, and very friendly. The assistant takes a deep breath and answers questions step by step.",
+                "content": "The following is a conversation with an AI assistant tasked with crafting tweets according to various requested levels of humor, vulgarity, and shock,"
             },
-            {"role": "user", "content": "Human: Hello, who are you?"},
-            {
-                "role": "assistant",
-                "content": "AI: I am an AI created by OpenAI. How can I help you today?",
-            },
-            {"role": "user", "content": f"Human: {content}"},
+            {"role": "user", "content": content},
         ],
     )
 
     response = completion.choices[0].message.content
-    response = self.extract_embedded_json(response)
-
+    
     return response
-"""
